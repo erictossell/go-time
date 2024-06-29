22b1066f50a0b7c7361b1bdbc915b0cbf83536cd
Eric Tossell <erictossell@gmail.com>
2024-06-29 11:27:48 -0400

    chore: hash update

--- default.nix
+++ default.nix
@@ -4,6 +4,6 @@ pkgs.buildGoModule {
   pname = "go-time";
   version = "0.1.58"; # Replace with your desired version
   src = ./.;
-  vendorHash = "sha256-aDCqZLQ1xoFGJFYbNa4TfXZJOIT0VT9CqK8nkf4+nPk=";
+  vendorHash = "sha256-RdEHYqMke/gOECqKntiuoxN1NajDgSN1MYIsjOvmW9c=";
 }
 
